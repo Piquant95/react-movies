49aabcf05abe564ce6c62688d0c2bce1c3a21854
Piquant95 <piquant95@yahoo.com>
2023-04-17 15:31:51 -0400

    created and imported data js file

--- src/pages/App/App.jsx
+++ src/pages/App/App.jsx
@@ -1,12 +1,12 @@
 import {useState} from "react";
 import { Routes, Route } from "react-router-dom";
 import './App.css';
-
 import LoginPage from '../LoginPage/LoginPage';
 import MoviesListPage from '../MoviesListPage/MoviesListPage';
 import MovieDetailPage from '../MovieDetailPage/MovieDetailPage';
 import ActorListPage from '../ActorListPage/ActorListPage';
 import NavBar from "../../components/NavBar/NavBar";
+import { movies } from "../../data.js";
 
 function App() {
   const [user, setUSer] = useState({});
